8d2308bfc8930f7cf99c0be0663994536e4f6651
angela <azheng8072@bths.edu>
2018-10-12 11:28:37 -0400

    added name info, main character, plot

--- src/ChatBlackButler_Angela.java
+++ src/ChatBlackButler_Angela.java
@@ -62,24 +62,25 @@ public class ChatBlackButler_Angela
 		}
 		// USE THIS AS EXAMPLE. you caN CHAnge goal to be a certain word in sentence to give answer.
         // response can be changed based on the found word
-        else if (chatBot.findKeyword(statement, "title") >= 0)
+        else if (chatBot.findKeyword(statement, "name") >= 0 || chatBot.findKeyword(statement, "title") >= 0 || chatBot.findKeyword(statement, "anime") >= 0)
 		{
-			response = "The name of this anime is Black Butler";
-        } else if (chatBot.findKeyword(statement, "no") >= 0)
+			response = "The name of this anime is Black Butler.\nBlack Butler is a Japanese manga series written and illustrated by Yana Toboso. \nIt debuted on September 16, 2006, it has been serialized in Square Enix's shōnen manga magazine Monthly GFantasy.";
+        }
+        else if (chatBot.findKeyword(statement, "plot") >= 0 || chatBot.findKeyword(statement, "what is it about") >= 0)
 		{
-			response = "Why so negative?";
+			response = "The plot is about a young boy sells his soul to a demon in order to avenge his family's death and successfully lead their influential toy manufacturing company. \nThe demon takes the form of a loyal butler who's always dressed in black and is required to protect, serve and arrive whenever summoned by his young master Ciel.";
                 	emotion--;
-        } else if (chatBot.findKeyword(statement, "levin") >= 0)
+        } else if (chatBot.findKeyword(statement, "main characters") >= 0 || chatBot.findKeyword(statement, "main") >= 0 || chatBot.findKeyword(statement, "character") >= 0)
 		{
-			response = "More like LevinTheDream, amiright?";
+			response = "Main characters are Sebastian Michaelis and Ciel Phantomhive.";
 			emotion++;
-        } else if (chatBot.findKeyword(statement, "folwell") >= 0)
+        } else if (chatBot.findKeyword(statement, "Ciel") >= 0 || chatBot.findKeyword(statement, "Phantomhive") >= 0)
 		{
-			response = "Watch your backpacks, Mr. Folwell doesn't fall well.";
+			response = "Ciel Phantomhive is a short, thirteen-year-old boy with bluish-black hair and rich, deep blue eyes.\nCiel nearly always wears a black eye-patch made of gauze with a single cord over his right eye to hide the location of his Faustian contract with Sebastian Michaelis. ";
 			emotion++;
-        } else if (chatBot.findKeyword(statement, "goldman") >= 0)
+        } else if (chatBot.findKeyword(statement, "Sebastian") >= 0 || chatBot.findKeyword(statement, "Michaelis") >= 0)
 		{
-			response = "Go for the gold, man.";
+			response = "Sebastian Michaelis takes on the appearance of a tall, handsome adult with black hair, red eyes, and pale skin. \nSebastian is a dedicated butler to Ciel Phantomhive and upholds a strong sense of duty to his master and household. \nHe faithfully follows and carries out every one of Ciel's orders, while ensuring that the Phantomhive household runs smoothly. ";
 			emotion++;
 		}
 
